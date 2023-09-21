f461d4153c3b33d465b410d6956a3cffab64b515
dylanzurita <dzuritag22@gmail.com>
2023-09-21 13:11:26 -0500

    guess number game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 50);
+        
+        int unumber = -1;
+             
+        while (unumber != number)
+        {
+            Console.Write("What is you guess? ");
+            unumber = int.Parse(Console.ReadLine()); 
+
+            if (unumber < number)
+            {
+                Console.WriteLine ("higher");
+            }
+            else if (unumber > number)
+            {
+                Console.WriteLine ("lower");
+            }
+            else
+            {
+                Console.WriteLine ("You guessed it");
+            }
+        }
     }
 }
