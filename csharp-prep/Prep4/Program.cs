ff6eb2ef83cf650ffc7c9f4a1f0cc43074e9f0be
dylanzurita <dzuritag22@gmail.com>
2023-09-21 22:09:43 -0500

    prep 5

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,44 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep4 World!");
+    {   
+        List<int> numbers = new List<int>();
+        int count = 0;
+        
+        int userNumber = -1; 
+        
+        while (userNumber != 0)
+        {       
+            Console.Write ("Enter number, type 0 when finished: ");
+            int number = int.Parse (Console.ReadLine());
+            
+            if (userNumber != 0)
+            {
+                numbers.Add(number);
+            }
+        } 
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+            count++;
+        }
+
+        int total = numbers.AsQueryable(). Sum();
+        Console.Write ($"The sum is: {total}");
+        int average = total / count;
+        Console.Write ($"The average is: {average}");
+        
+
     }
 }
+
+
+
+// do
+        // {
+        //     int userNumber = 0;
+        //     while (userNumber == 0)
+        // } while (true);
