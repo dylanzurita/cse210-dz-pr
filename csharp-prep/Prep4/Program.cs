55542f77c39800c5132c1e13db45a13786b9b676
dylanzurita <dzuritag22@gmail.com>
2023-09-21 22:15:48 -0500

    prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,37 +4,49 @@ using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
-    {   
+    {
         List<int> numbers = new List<int>();
-        int count = 0;
         
-        int userNumber = -1; 
-        
+        int userNumber = -1;
         while (userNumber != 0)
-        {       
-            Console.Write ("Enter number, type 0 when finished: ");
-            int number = int.Parse (Console.ReadLine());
+        {
+            Console.Write("Enter a number (0 to quit): ");
+            
+            string userResponse = Console.ReadLine();
+            userNumber = int.Parse(userResponse);
             
             if (userNumber != 0)
             {
-                numbers.Add(number);
+                numbers.Add(userNumber);
             }
-        } 
-        foreach (int number in numbers)
-        {
-            Console.WriteLine(number);
-            count++;
         }
 
-        int total = numbers.AsQueryable(). Sum();
-        Console.Write ($"The sum is: {total}");
-        int average = total / count;
-        Console.Write ($"The average is: {average}");
-        
 
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"The max is: {max}");
     }
 }
-
 
 
 // do
