e56ca1e2a682507cb005b01911fb595fb9bf75c8
dylanzurita <dzuritag22@gmail.com>
2023-10-27 16:32:29 -0500

    learning 04 assignment

--- prepare/Learning04/MathAssignment.cs
+++ prepare/Learning04/MathAssignment.cs
@@ -1,17 +1,34 @@
-public class MathAssignment : Assignment
+public class MathAssignment: Assignment
 {
-    private string _textbookSection;
-    private string _problems;
+    private string _textbookSection = " ";
+    private string _problems = "";
 
-    public MathAssignment(string studentName, string topic, string textbookSection, string problems)
-        : base(studentName, topic)
+    public MathAssignment (string studentName, string topic, string textbookSection, string problems) : base(studentName, topic)
     {
         _textbookSection = textbookSection;
         _problems = problems;
     }
 
-    public string GetHomeworkList()
+    public string GetHomeworkList() 
     {
         return $"Section {_textbookSection} Problems {_problems}";
     }
 }
+
+// public class MathAssignment : Assignment
+// {
+//     private string _textbookSection;
+//     private string _problems;
+
+//     public MathAssignment(string studentName, string topic, string textbookSection, string problems)
+//         : base(studentName, topic)
+//     {
+//         _textbookSection = textbookSection;
+//         _problems = problems;
+//     }
+
+//     public string GetHomeworkList()
+//     {
+//         return $"Section {_textbookSection} Problems {_problems}";
+//     }
+// }
--- prepare/Learning04/WritingAssignment.cs
+++ prepare/Learning04/WritingAssignment.cs
@@ -1,17 +1,31 @@
 public class WritingAssignment : Assignment
 {
-    private string _title;
+    private string _title = " ";
 
-    public WritingAssignment(string studentName, string topic, string title)
-        : base(studentName, topic)
+    public WritingAssignment (string studentName, string topic, string title) : base (studentName, topic)
     {
         _title = title;
     }
-
-    public string GetWritingInformation()
+    public string GetWritingInformation () 
     {
         string studentName = GetStudentName();
-
         return $"{_title} by {studentName}";
     }
 }
+// public class WritingAssignment : Assignment
+// {
+//     private string _title;
+
+//     public WritingAssignment(string studentName, string topic, string title)
+//         : base(studentName, topic)
+//     {
+//         _title = title;
+//     }
+
+//     public string GetWritingInformation()
+//     {
+//         string studentName = GetStudentName();
+
+//         return $"{_title} by {studentName}";
+//     }
+// }
